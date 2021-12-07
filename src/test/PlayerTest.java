22d1a2154f6eaa98b69647c64c51cc40ba653a97
Alexander Garcia <32944795+iamgarcia@users.noreply.github.com>
2021-12-06 17:35:45 -0800

    Add test folder

---
+++ src/test/PlayerTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setThrusting() {
+    }
+
+    @Test
+    public void setRotateLeft() {
+    }
+
+    @Test
+    public void setRotateRight() {
+    }
+
+    @Test
+    public void setFiring() {
+    }
+
+    @Test
+    public void setFiringEnabled() {
+    }
+}
